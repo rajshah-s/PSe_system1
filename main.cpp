9d326bca6828e118625bc9bba362ce550789e214
Raj <faceboy100@gmail.com>
2025-03-13 16:46:01 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+// Stap 1.1
+unordered_map<string, vector<string>> elementAttributes = {
+    {"BAAN", {"naam", "lengte"}},
+    {"VERKEERSLICHT", {"baan", "positie", "cyclus"}},
+    {"VOERTUIG", {"baan", "positie"}},
+    {"VOERTUIGGENERATOR", {"baan", "frequentie"}}
+};
